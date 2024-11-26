4d40f7c5019549889f3ccc574e20856cf431ac28
Sina <sinaderik7@gmail.com>
2024-11-26 09:38:49 +0330

    material ui is configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Button from '@mui/material/Button';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello</h1>
+    <div className="flex items-center gap-3">
+      <h1 className="text-3xl font-bold underline mr-4">Hello</h1>
+      <Button variant="contained">Contained</Button>
     </div>
   );
 };
