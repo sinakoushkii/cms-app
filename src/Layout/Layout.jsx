3a9dd51bde19b2ca6b3352fcd2ec1ab0ff4e0cd5
Sina <sinaderik7@gmail.com>
2024-11-26 10:45:22 +0330

    header component is created

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
 
 const Layout = () => {
   return (
     <div>
-        <h3>header</h3>
+        <Header />
         <div>
             <Outlet />
         </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Header styled</div>
+  )
+}
+
+export default Header
